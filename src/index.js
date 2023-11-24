556968c2316255e2ee28035fec488cd73713864d
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-24 16:54:55 -0300

    Configure eslint and editorConfig

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.listen(3000, () =>
-  console.log("🔥 Server started at http://localhost:3000")
-);
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.get('/', (request, response) => {
+  response.send('Hello World');
 });
