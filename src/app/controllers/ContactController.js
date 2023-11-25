8d4eae6ad372ffc660b0caa0c0ea4827f6082c4d
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-24 21:21:48 -0300

    Implement new updates

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index(request, response) {
+    // Listar todos os registros
+    response.send('Send from Contact Controller');
+  }
+
+  show() {
+    // Obter um registro
+  }
+
+  store() {
+    // Criar novo registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+module.exports = new ContactController();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
