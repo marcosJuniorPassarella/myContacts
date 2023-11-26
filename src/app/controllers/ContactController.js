89c7881304e198a94b2a5b9000d0311a6db06e6a
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-26 16:05:32 -0300

    Implement show and delete repository methods

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,15 +1,23 @@
+const { response } = require("express");
 const ContactsRepository = require("../repositories/ContactsRepository");
 
 class ContactController {
-
   async index(request, response) {
     // Listar todos os registros
     const contacts = await ContactsRepository.findAll();
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
     // Obter um registro
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: Not Found
+      return response.status(404).json({ error: "User not found" });
+    }
+    response.json(contact);
   }
 
   store() {
@@ -20,8 +28,18 @@ class ContactController {
     // Editar um registro
   }
 
-  delete() {
+  async delete(request, response) {
     // Deletar um registro
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: Not Found
+      return response.status(404).json({ error: "User not found" });
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204); // 204 Deu tudo certo porém a resposta não tem conteúdo a ser retornado
   }
 }
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,11 +1,18 @@
 const { v4 } = require("uuid");
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: "Matheus",
     email: "matheus@mail.com",
     phone: "242323432",
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "Marcos",
+    email: "marcos@mail.com",
+    phone: "323131312",
     category_id: v4(),
   },
 ];
@@ -14,6 +21,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve, reject) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve, reject) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
