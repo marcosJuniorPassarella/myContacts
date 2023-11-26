d46872bfd6bc69e5fed5f3134464589ed873220b
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-26 15:13:18 -0300

    Remove uuidv4 package and install uuid

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require("uuidv4");
+const { v4 } = require("uuid");
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: "Matheus",
     email: "matheus@mail.com",
     phone: "242323432",
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
