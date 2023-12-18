d5169ecbbb582789aa7607d1abf6b58daffbc9b9
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-12-18 19:58:13 -0300

    Implement create category feature

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,20 @@
+const CategoriesRepository = require("../repositories/CategoryRepository");
+
+class CategoryController {
+  index(request, response) {
+    response.send("ok - index");
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: "Name is required" });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+    response.json(category);
+  }
+}
+
+module.exports = new CategoryController();
---
+++ src/app/repositories/CategoryRepository.js
@@ -0,0 +1,19 @@
+const db = require("../../database");
+
+class CategoriesRepository {
+  findAll() {}
+
+  async create({ name }) {
+    const [row] = await db.query(
+      `
+    INSERT INTO categories(name)
+    VALUES($1)
+    RETURNING *
+    `,
+      [name]
+    );
+    return row;
+  }
+}
+
+module.exports = new CategoriesRepository();
