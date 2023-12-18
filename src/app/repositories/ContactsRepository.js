fe12ed4954599e000ec04e7e20dff588a907d1fb
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-12-18 18:17:55 -0300

    Finish refactor create method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -45,11 +45,15 @@ class ContactsRepository {
   }
 
   async create({ name, email, phone, category_id }) {
-    const row = await db.query(
+    const [row] = await db.query(
       `INSERT INTO contacts(name, email, phone, category_id)
-       VALUES($1, $2, $3, $4)`,
+       VALUES($1, $2, $3, $4)
+       RETURNING *
+       `,
       [name, email, phone, category_id]
     );
+
+    return row;
   }
 
   update(id, { name, email, phone, category_id }) {
