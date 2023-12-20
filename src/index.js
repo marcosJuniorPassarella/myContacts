5d260d1d5c6bb59d607af9b46619d0345938c542
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-12-20 17:35:25 -0300

    Iplement error handler middleware and install express-async-errors package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+// Error Handler Middleware
+app.use((error, request, response, next) => {
+  console.log("Error Handler");
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () =>
   console.log("🔥 Server started at http://localhost:3000")
